72393980429cdaaeceb84b53658132513c4aaa00
blockchain-bot-father <du9336620@gmail.com>
2025-10-02 09:39:35 +0800

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+module.exports = {
+  BSC_RPC: process.env.BSC_RPC,
+  BSC_V2_FACTORY: process.env.BSC_V2_FACTORY,
+  BSC_V3_FACTORY: process.env.BSC_V3_FACTORY,
+  TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN,
+  TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID
+};
